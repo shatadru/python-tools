ae49b9a635d1cc4542513bc79c4be97180cc95a8
Shatadru Bandyopadhyay <shatadru1@gmail.com>
2020-03-16 20:42:47 +0530

    Add OverallStats to add global statistics

--- covid19.py
+++ covid19.py
@@ -1,6 +1,8 @@
 import pandas as pd
 import numpy as np
+from requests_html import HTML
 import requests
+
 
 url = "https://www.worldometers.info/coronavirus/"
 
@@ -17,35 +19,31 @@ class FetchData:
         #}
         header = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
 
-        #session = HTMLSession()
         try:
             r = requests.get(url, headers=header)
-        #    page = session.get(url, headers=header)
-            #print(r.content)
+            page = r.content
         except OSError as err:
             print("Unable to reach www.worldometers.info/coronavirus : Error is : " + str(err))
+            exit(254)
+        return page
+
+class CountryStats:
+    def __init__(self, page, country_name):
+        self.page=page
+        self.country_name=country_name
 
 
-        # news =  page.html.find('#innercontent', first=True)
-
+    def get_covid19_country_stats(self):
         try:
-            nan_tables = pd.read_html(r.content)
+            nan_tables = pd.read_html(self.page)
         except Exception as err:
             print ("Error is +", str(err))
             exit(255)
         tables = nan_tables[0].replace(np.nan, 0)
         table = tables.values
-        return table
 
-class CountryStats:
-    def __init__(self, table, country_name):
-        self.table=table
-        self.country_name=country_name
-
-    def get_covid19_country_stats(self):
         found = 0
-        #print(table)
-        for row in self.table:
+        for row in table:
             if row[0].lower() == self.country_name.lower():
                 cases = {}
                 cases["Country"] = str(row[0])
@@ -61,19 +59,39 @@ class CountryStats:
                 else:
                     cases["Mortality ratio"] = 0
 
-
-                #print("Serious Critical: ", int(row[7]) )
-                #print("----------------------------------")
                 found = 1
                 return(cases)
 
-
         if found != 1 :
-           return "Country not affected by Covid19 or may be misspelled country name?"
+           return 255
+
+class OverallStats:
+    def __init__(self, page):
+        self.page=page
+        cases = {}
+
+    def get_overall_stats(self):
+
+        html = HTML(html=self.page)
+        counters = html.find('#maincounter-wrap')
+        main_counter = []
+        cases = {}
+        for entry in counters:
+            main_counter.append((int(entry.text.split('\n')[1].replace(',', ''))))
+
+        cases["Total cases"] = main_counter[0]
+        cases["Total recovered"] = main_counter[2]
+        cases["Total deaths"] = main_counter[1]
+        cases["Active cases"] = cases["Total cases"]  - cases["Total recovered"] - cases["Total deaths"]
+        cases["Mortality ratio"] = round((cases["Total deaths"] * 100 / cases["Total cases"] ),2)
+        return(cases)
+
 fd=FetchData(url)
-table=fd.get_data()
+page=fd.get_data()
 
 country=input("Please enter country name : ")
-p = CountryStats(table,country)
-print(p.get_covid19_country_stats())
+p1 = CountryStats(page, country)
+print(p1.get_covid19_country_stats())
 
+p2 = OverallStats(page)
+print(p2.get_overall_stats())
